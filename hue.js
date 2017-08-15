2188d03ce3a62514dadf6ce5cb53a3ff1b7070c9
Yoidea <yoidea@Yoidea-iMac.local>
2017-08-15 23:40:14 +0900

    Add new function

--- hue.js
+++ hue.js
@@ -1,33 +1,41 @@
-function hue(name){
+function hueRequest(lights, on, bri, sat, hue){
 	// 要求先の設定
 	const ip = 'IP Address'; // HueのIPアドレス
 	const uri = 'api';
 	const key = 'API Key'; // HueのAPIキー
+	var toData;
+	if (on){
+		toData = '{"on":true, ';
+		toData += '"bri":' + bri + ',';
+		toData += '"sat":' + sat + ',';
+		toData += '"hue":' + hue + '}';
+	} else {
+		toData = '{"on":false}';
+	}
+	// Ajaxリクエスト作成
+	var settings = {
+		type: 'PUT',
+		url: 'http://' + ip + '/' + uri + '/' + key + '/lights/' + lights + '/state',
+		data: toData,
+		dataType: 'json',
+		success: function(response){
+			console.log(response);
+		},
+		error: function(req, err){
+			console.log(err);
+		}
+	};
+	// Ajaxリクエスト送信
+	$.ajax(settings);
+}
+
+function hue(name){
 	// Recipesから個別のライト設定を参照
 	for (var i in hueRecipes[name]){
-		var toData;
 		if (hueRecipes[name][i].on){
-			toData = '{"on":true, ';
-			toData += '"bri":' + hueRecipes[name][i].bri + ',';
-			toData += '"sat":' + hueRecipes[name][i].sat + ',';
-			toData += '"hue":' + hueRecipes[name][i].hue + '}';
+			hueRequest(i, true, hueRecipes[name][i].bri, hueRecipes[name][i].sat, hueRecipes[name][i].hue);
 		} else {
-			toData = '{"on":false}';
+			hueRequest(i, false);
 		}
-		// Ajaxリクエスト作成
-		var settings = {
-			type: 'PUT',
-			url: 'http://' + ip + '/' + uri + '/' + key + '/lights/' + i + '/state',
-			data: toData,
-			dataType: 'json',
-			success: function(response){
-				console.log(response);
-			},
-			error: function(req, err){
-				console.log(err);
-			}
-		};
-		// Ajaxリクエスト送信
-		$.ajax(settings);
 	}
 }
