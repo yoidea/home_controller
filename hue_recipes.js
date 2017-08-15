1cedf03835573ccb2cdbe607d8f4d8998a12e01c
Yoidea <yoidea@Yoidea-iMac.local>
2017-08-15 22:54:30 +0900

    Add recipe files

--- hue_recipes.js
+++ hue_recipes.js
@@ -3,24 +3,30 @@ var hueRecipes = {
         "1": {
             "on": true,
             "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "hue": 14974,
+            "sat": 140
         },
         "2": {
             "on": true,
             "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "hue": 14974,
+            "sat": 140
+        },
+        "3": {
+            "on": true,
+            "bri": 254,
+            "hue": 14974,
+            "sat": 140
         },
         "4": {
             "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "bri": 102,
+            "hue": 13201,
+            "sat": 228
         },
         "5": {
             "on": true,
-            "bri": 254,
+            "bri": 152,
             "hue": 8381,
             "sat": 141
         },
@@ -28,76 +34,82 @@ var hueRecipes = {
     "relax": {
         "1": {
             "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "bri": 140,
+            "hue": 13524,
+            "sat": 200
         },
         "2": {
             "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "bri": 140,
+            "hue": 13524,
+            "sat": 200
+        },
+        "3": {
+            "on": true,
+            "bri": 140,
+            "hue": 13524,
+            "sat": 200
         },
         "4": {
             "on": true,
             "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "hue": 13124,
+            "sat": 231
         },
         "5": {
             "on": true,
             "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "hue": 7676,
+            "sat": 199
         },
     },
-    "concentrate": {
+    "energize": {
         "1": {
             "on": true,
             "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "hue": 33016,
+            "sat": 53
         },
         "2": {
             "on": true,
             "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "hue": 33016,
+            "sat": 53
+        },
+        "3": {
+            "on": true,
+            "bri": 254,
+            "hue": 33016,
+            "sat": 53
         },
         "4": {
             "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "bri": 127,
+            "hue": 44876,
+            "sat": 127
         },
         "5": {
             "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "bri": 203,
+            "hue": 44568,
+            "sat": 254
         },
     },
     "back": {
         "1": {
-            "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "on": false
         },
         "2": {
-            "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "on": false
+        },
+        "3": {
+            "on": false
         },
         "4": {
-            "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "on": false
         },
         "5": {
-            "on": true,
+            "on": false,
             "bri": 254,
             "hue": 8381,
             "sat": 141
@@ -105,80 +117,71 @@ var hueRecipes = {
     },
     "night": {
         "1": {
-            "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "on": false
         },
         "2": {
-            "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "on": false
+        },
+        "3": {
+            "on": false
         },
         "4": {
-            "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "on": false
         },
         "5": {
             "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "bri": 1,
+            "hue": 13124,
+            "sat": 231
         },
     },
     "mood": {
         "1": {
             "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "bri": 50,
+            "hue": 0,
+            "sat": 200
         },
         "2": {
             "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "bri": 50,
+            "hue": 0,
+            "sat": 200
+        },
+        "3": {
+            "on": true,
+            "bri": 50,
+            "hue": 0,
+            "sat": 200
         },
         "4": {
             "on": true,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "bri": 178,
+            "hue": 49965,
+            "sat": 247
         },
         "5": {
             "on": true,
             "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "hue": 60600,
+            "sat": 199
         },
     },
     "off": {
         "1": {
-            "on": false,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "on": false
         },
         "2": {
-            "on": false,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "on": false
+        },
+        "3": {
+            "on": false
         },
         "4": {
-            "on": false,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "on": false
         },
         "5": {
-            "on": false,
-            "bri": 254,
-            "hue": 8381,
-            "sat": 141
+            "on": false
         }
     }
 }
